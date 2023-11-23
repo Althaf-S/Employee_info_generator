a820dd5f7be26fefe1cbad7e98102d4f517dcf7c
Althaf S <althaf.sabsara@gmail.com>
2023-11-24 00:15:44 +0530

    Removed unwanted code

--- genvcf.py
+++ genvcf.py
@@ -66,32 +66,7 @@ def parse_args():
     #Generate details of employees leaves on csv file
     parser_initcsv = subparser.add_parser("rtrcsv",help="Generate details of employees leaves on csv file")
     
-    #parser_load.add_argument("-n", "--number", help="Number of records to generate", action='store', type=int, default=100)
-    #parser_load.add_argument("-s", "--sizeqr", help="Size of qr code png", action='store', type=int, default=500)
-    #parser_load.add_argument("-q", "--qrcode", help="Generate both qrcode and vcard files", action='store_true', default= False )
-    #parser_load.add_argument("-a", "--address", help="Provide an address other than default address", action='store', default= '100 Flat Grape Dr.;Fresno;CA;95555;United States of America' )
-    
-    
-    #retrieve count from new table 
-    parser_ct = subparser.add_parser("rtrnt",help="Retrieve count of absents")
-    parser_ct.add_argument("-u","--user", help="Adding user name of database",action="store",type=str,default='althaf')
-    parser_ct.add_argument("-d","--dbname", help="Adding database name",action="store",type=str,default='employee')
-    parser_ct.add_argument("-i","--userid", help="id of the user whose leave data needs to be obtained",action="store",type=int,default=1)
-    
-    #adding data to leaves table
-    parser_file = subparser.add_parser("adddata",help="Adding data to leaves table")
-    parser_file.add_argument("-u","--user", help="Adding user name of database",action="store",type=str,default='althaf')
-    parser_file.add_argument("-d","--dbname", help="Adding database name",action="store",type=str,default='employee')
-    parser_file.add_argument("-f","--filename", help="id of the user whose leave data needs to be obtained",action="store",type=str,default='leaves.sql')  
-    
-    #adding data to leaves table witthout sql file
-    parser_sq = subparser.add_parser("sql",help="Adding data to leaves table")
-    parser_sq.add_argument("-u","--user", help="Adding user name of database",action="store",type=str,default='althaf')
-    parser_sq.add_argument("-d","--dbname", help="Adding database name",action="store",type=str,default='employee')
-    parser_sq.add_argument("-t","--date", help="Input data to add it to second table", action="store",type=str,default='0000-00-00')
-    parser_sq.add_argument("-i","--id", help="Input data of user id", action="store",type=int,default=0)     
-    parser_sq.add_argument("-r","--reason", help="Input data regarding cause", action="store",type=str,default='NONE')
-    
+
     
     
     args = parser.parse_args() 
@@ -112,14 +87,10 @@ def logger(is_logger):
     logger.addHandler(handler)
 
 
-#def data_from_details(details,number):
-#  data = []
-#  for i in range(0,number):
-#     data.append(details[i])
-#  return data
-
 
 # Database implementation starts
+
+
 
 #def create_database(user,dbase):
 #  conn = pg.connect(database="postgres",user=user)
@@ -129,9 +100,6 @@ def logger(is_logger):
 #  cursor.execute(create_database)
 #  print("Creation of database is successful")
 #  conn.close()
-
-
-
 
 
 ## Clarified error and built new function to create table
@@ -302,61 +270,7 @@ def generate_leave_csv(args):
   cursor.execute(insert_info,(args.designation,args.numoflv))
   conn.commit()
   print("data inserted")
-  conn.close()
-  
-
-#Implementation of details table ends
-
-#def truncate_table_leaves(user,dbname,filename):
-#  with open(f"{filename}",'w+') as f:
-#     f.close()
-#  conn = pg.connect(f"dbname={dbname} user={user}")
-#  cursor = conn.cursor()
-#  truncate_table = "TRUNCATE TABLE leaves RESTART IDENTITY CASCADE"
-#  cursor.execute(truncate_table)
-#  conn.commit()
-#  conn.close()
-
-  
-#def create_new_table(user,dbname):
-#  #truncate_table_leaves(user,dbname)
-#  conn = pg.connect(f"dbname={dbname} user={user}")
-#  cursor = conn.cursor()
-#  create_table = """CREATE TABLE leaves (serial_number SERIAL PRIMARY KEY,
-#                                         date DATE,
-#                                         employee_id INTEGER REFERENCES details(serial_number),
-#                                       reason VARCHAR(50),UNIQUE (date,employee_id))"""
-#  cursor.execute(create_table)
-#  print("New table created successfully")
-#  conn.commit()
-#  conn.close()
-  
-#def add_new_data_to_new_table(user,dbname,filename):
-#  conn = pg.connect(f"dbname={dbname} user={user}")
-#  cursor = conn.cursor()
-#  with open(f"{filename}",'r') as f:
-#    cursor.execute(f.read())
-#    conn.commit()
-# conn.close()
-# f.close()
-  
-  
-#def add_data_to_new_table(user,dbname,date,employee_id,reason):
-#  conn = pg.connect(f"dbname={dbname} user={user}")
-#  cursor = conn.cursor()
-#  insert_info = """INSERT INTO leaves (date,empoyee_id,reason) VALUES (%s,%d,%s)"""
-#  cursor.execute(insert_info,(date,employee_id,reason))
-#  conn.commit()
-#  print("data inserted")
-#  conn.close()
-  
-  
-
-  
-  
-  
-  
-  
+  conn.close()  
   
   
 # Database implementation ends
@@ -377,18 +291,6 @@ END:VCARD
 """
   
 
-
-#def generate_vcf(details,address):
-#  if not os.path.exists('worker_vcf'):
-#    os.mkdir('worker_vcf')
-#  count = 1
-#  for first_name,last_name,job,email,ph_no in details:
-#       imp_vcard = implement_vcf(first_name,last_name,job,email,ph_no,address)
-#       logger.debug("Writing row %d", count)
-#       count +=1
-#       with open(f'worker_vcf/{email}.vcf','w') as j:
-#          j.write(imp_vcard)
-#  logger.info("Done generating vCards")  
      
        
   
@@ -406,19 +308,6 @@ REV:20150922T195243Z
 END:VCARD""")
    return reqs.content
        
-
-       
-#def generate_qrcode(details,size,address):
-#  if not os.path.exists('worker_vcf'):
-#    os.mkdir('worker_vcf')
-#  count = 1
-#  for first_name,last_name,job,email,ph_no in details:
-#    imp_qrcode = implement_qrcode(first_name,last_name,job,email,ph_no,size,address)
-#    logger.debug("Writing row %d", count)
-#    count +=1
-#    with open(f'worker_vcf/{email}.qr.png','wb') as f:
-#      f.write(imp_qrcode)
-#  logger.info("Done generating qr code files")
   
   
 def main():
@@ -435,24 +324,7 @@ def main():
                    "rtrcsv"   : generate_leave_csv
                  }
     operations[args.subcommand](args)
-    #if args.subcommand == "inittb":
-      #create_table(args)
-    #if args.subcommand == "load":
-      #add_data_to_table_details(args)
-    #if args.subcommand == "rtr":
-      # retriving_data_from_database(args)
-       #if args.qrcode:
-        #  generate_qrcode(details,args.sizeqr,args.address)
-    if args.subcommand == "createtb":
-       create_new_table(args.user,args.dbname)
-    if args.subcommand == "rtrct":
-       retrive_data_from_new_table(args.user,args.dbname,args.userid)
-    if args.subcommand == "adddata":
-       add_new_data_to_new_table(args.user,args.dbname,args.filename)
-    if args.subcommand == "trct":
-       truncate_table_leaves(args.user,args.dbname,args.filename)
-    if args.subcommand == "sql":
-       add_data_to_new_table(args.user,args.dbname,args.date,args.id,args.reason)
+
   except HRException as e:
      logger.error("Program aborted due to %s", e)
      sys.exit(-1)
