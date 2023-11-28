b4557846e4b0db8691a1b76a93011af800f3685f
Althaf S <althaf.sabsara@gmail.com>
2023-11-28 15:14:15 +0530

    Corrects test file

--- test_genvcf.py
+++ test_genvcf.py
@@ -1,42 +1,29 @@
 import csv
 import os
+
 import genvcf
 
-def test_details_from_csv():
-  result = genvcf.details_from_csv('test_data.csv')
-  assert result == [['Lopez', 'Kathy', 'Horticulturist, amenity', 'kathy.lopez@warren.org', '001-383-311-4585'], 
-                    ['Lawson', 'Kristy', 'Quantity surveyor', 'krist.lawso@brown-robinson.com', '372.280.1290']]
-                    
+
 def test_generate_vcs():
   with open('kathy.lopez@warren.org.vcf','w') as f:
     f.write(genvcf.implement_vcf('Lopez', 'Kathy', 'Horticulturist, amenity', 'kathy.lopez@warren.org', '001-383-311-4585'))
   with open('kathy.lopez@warren.org.vcf', 'r') as f:
     vcard = f.read()
-  assert f"""BEGIN:VCARD
+    assert """\nBEGIN:VCARD
 VERSION:2.1
-N:Kathy;Lopez
-FN:Lopez Kathy
+N:Lopez;Kathy
+FN:Kathy Lopez
 ORG:Authors, Inc.
 TITLE:Horticulturist, amenity
 TEL;WORK;VOICE:001-383-311-4585
 ADR;WORK:;;100 Flat Grape Dr.;Fresno;CA;95555;United States of America
 EMAIL;PREF;INTERNET:kathy.lopez@warren.org
 REV:20150922T195243Z
-END:VCARD
-"""              in    vcard
+END:VCARD\n"""   ==    vcard
   os.unlink('kathy.lopez@warren.org.vcf')
+  
+
    
-def csv_test_data():
-   names = [['Lopez', 'Kathy', 'Horticulturist, amenity', 'kathy.lopez@warren.org', '001-383-311-4585'], 
-             ['Lawson', 'Kristy', 'Quantity surveyor', 'krist.lawso@brown-robinson.com', '372.280.1290']]
-   with open('test_data.csv', 'w', newline='') as csvfile:
-     writer = csv.writer(csvfile)
-     for i in names:
-       writer.writerow(i)
-   return csvfile
 
-
-
- 
 
 
