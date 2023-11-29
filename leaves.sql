a52b815cc0977d92bb47f34fa090bc19f3e363c0
Althaf S <althaf.sabsara@gmail.com>
2023-11-29 15:48:01 +0530

    Removes unwanted files from repo

--- leaves.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO leaves (date,employee_id,reason) VALUES ('2022-10-10',4,'fever'),('2022-10-07',4,'cold');
-INSERT INTO leaves (date,employee_id,reason) VALUES ('2022-10-10',3,'cold');
-
-       
-                  
-
