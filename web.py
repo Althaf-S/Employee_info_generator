b9c3341be6fa8d444444adddd4aa0da18ce87bb5
Althaf S <althaf.sabsara@gmail.com>
2023-12-07 00:11:42 +0530

    Adds option to see list of user and details side by side

--- web.py
+++ web.py
@@ -1,5 +1,5 @@
 import flask
-
+from flask import render_template,request,redirect,url_for
 
 import models
 from sqlalchemy import func
@@ -20,26 +20,24 @@ def employees():
   query = db.select(models.employee).order_by(models.employee.firstname)
   users =db.session.execute(query).scalars()
   return flask.render_template("userlist.html",users = users)
-  
-@app.route("/employees/<int:empid>")
+
+@app.route("/employees/<int:empid>", methods=["GET", "POST"])
 def employee_details(empid):
+  all_user_query = db.select(models.employee).order_by(models.employee.firstname)
+  all_user =db.session.execute(all_user_query).scalars()
   query = db.select(models.employee).where(models.employee.empid == empid)
   user = db.session.execute(query).scalar()
   query1 = db.select(func.count(models.employee.empid)).join(models.leaves,models.employee.empid == models.leaves.empid).where(models.employee.empid==empid)
   leaves = db.session.execute(query1).scalar()
   query2 = db.select(models.designation.max_leaves).where(models.designation.jobid == models.employee.title_id, models.employee.empid==empid)
   max_leaves = db.session.execute(query2).scalar()
-  return flask.render_template("userdetails.html", user = user, leaves = leaves, max_leaves = max_leaves)
-  
-@app.route("/addleave",methods=["GET","POST"])
-def add_leaves():
-  if flask.request.method == "POST":
-    empid = flask.request.form['empid']
-    date = flask.request.form['date']
-    reason = flask.request.form['reason'] 
-    query = models.leaves(empid=empid,date=date,reason=reason)
-    db.session.add(query)
+  if request.method == "POST":
+    date = request.form['date']
+    reason = request.form['reason'] 
+    query3 = models.leaves(empid=empid ,date=date, reason=reason)
+    db.session.add(query3)
     db.session.commit()
-    return flask.render_template('/')
-  return flask.render_template("addleave.html")
+    return redirect(url_for("employee_details",empid=empid))
+  return render_template("userdetails.html", user=user, leaves=leaves, max_leaves=max_leaves, all_user=all_user)
+
   
