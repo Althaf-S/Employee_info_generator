bfce41787dbc75d1a144ecc6b12876575ae3062d
Althaf S <althaf.sabsara@gmail.com>
2023-12-18 13:03:31 +0530

    Edits web.py to handle json leave data

--- web.py
+++ web.py
@@ -1,11 +1,13 @@
 import flask
 from flask import render_template,request,redirect,url_for
+from flask_cors import CORS
 
 import models
 from sqlalchemy import func
 
 
 app = flask.Flask("hrms")
+CORS(app)
 db = models.SQLAlchemy(model_class = models.HRDBBase)
 
 @app.route("/",methods = ["GET","POST"])
@@ -17,8 +19,17 @@ def index():
 def employees():
     employees = db.select(models.Employee).order_by(models.Employee.firstname)
     users = db.session.execute(employees).scalars()
-    return flask.render_template("userlist.html", users = users)
-
+    ret = []
+    for user in users:
+      details = {"id" : user.empid,
+         "firstname" : user.firstname,
+         "lastname" : user.lastname,
+         "title" : user.title.title,
+         "email" : user.email,
+         "phone" : user.ph_no
+         }
+      ret.append(details)
+    return flask.jsonify(ret)
   
 @app.route("/employees/<int:empid>")
 def employee_details(empid):
@@ -28,14 +39,16 @@ def employee_details(empid):
   leaves = db.session.execute(emp_count).scalar()
   leave_count = db.select(models.Designation.max_leaves).where(models.Designation.jobid == models.Employee.title_id, models.Employee.empid==empid)
   max_leaves = db.session.execute(leave_count).scalar()
-  ret = {"employee_id" : user.empid,
+  ret = []
+  details = {"employee_id" : user.empid,
          "firstname" : user.firstname,
          "lastname" : user.lastname,
          "title" : user.title.title,
          "email" : user.email,
          "phone" : user.ph_no,
          "leaves": leaves,
-         "max_leaves" : max_leaves}         
+         "max_leaves" : max_leaves}   
+  ret.append(details)      
   return flask.jsonify(ret)
 
 @app.errorhandler(500)   
@@ -46,20 +59,22 @@ def page_not_found(error):
 def page_not_found(error):
     return render_template('404.html'), 404
 
-@app.route("/addleave/<int:empid>", methods=["GET", "POST"])
+@app.route("/leave/<int:empid>", methods=["GET", "POST"])
 def addleave(empid):
   employees = db.select(models.Employee).where(models.Employee.empid == empid)
   user = db.session.execute(employees).scalar()
   if request.method == "POST":
-    date = request.form['date']
-    reason = request.form['reason'] 
+    data = request.get_json()
+    date = data.get('date')
+    reason = data.get('reason')
+    if not date or not reason:
+       return flask.jsonify({'error': 'Enter data'}), 400
     insert_data = models.Leaves(empid=empid ,date=date, reason=reason)
     db.session.add(insert_data)
     db.session.commit()
-    return redirect(url_for("employees"))
+    return flask.jsonify({'message': 'Leave submitted successfully'}), 200
+  return flask.jsonify({'error': 'Method Not Allowed'}), 405
   
-@app.route("/about")
-def about():
-  return render_template("about.html")
+
 
   
